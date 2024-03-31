5a78e5ff149813f5e12e25fc6e0173e44cd47fde
isthistaken3 <148587538+isthistaken3@users.noreply.github.com>
2024-03-31 18:22:23 -0400

    Delete myscript.js

--- myscript.js
+++
@@ -1,9 +0,0 @@
-  function checkPassword() {
-   var password = document.getElementById("passwordBox");
-   var passwordText = password.value;
-   if(passwordText == "love you andrewos") {
-    return true;
-   }
-   alert("Access denied! Incorrect password!");
-   return false;
-   }
